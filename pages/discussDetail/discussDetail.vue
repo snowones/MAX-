6c8b3fb6a1aacd63966c7f62e06c372f9b68a5c8
张应祥 <37978150+snowones@users.noreply.github.com>
2020-04-28 16:53:33 +0800

    帖子详情页面实现（纯静态）

--- pages/discussDetail/discussDetail.vue
+++ pages/discussDetail/discussDetail.vue
@@ -6,6 +6,7 @@
 			<block slot="content">主题帖</block>
 		</cu-custom>
 		<!-- 切换导航 -->
+		
 		<swiper class="screen-swiper round-dot" :indicator-dots="true" :circular="true" :autoplay="true" interval="5000"
 		 duration="500">
 			<swiper-item v-for="(item,index) in data.contentImg" :key="index">
@@ -13,6 +14,91 @@
 			</swiper-item>
 		</swiper>
 		
+		<view class="cu-card dynamic" :class="isCard?'no-card':''">
+			<view class="cu-item shadow">
+				<view class="cu-list menu-avatar">
+					<view class="cu-item">
+						<view class="cu-avatar round lg" :style='"background-image:url(" + data.user_avatar + ");"'></view>
+						<view class="content flex-sub">
+							<view>{{data.userName}}</view>
+							<view class="text-gray text-sm flex justify-between">
+								{{data.title}}
+							</view>
+						</view>
+					</view>
+				</view>
+				<view class="text-content">
+					{{data.content}}
+				</view>
+				<view class="text-gray text-sm text-right padding">
+					<text class="cuIcon-attentionfill margin-lr-xs"></text> {{data.vivewNumber}}
+					<text class="cuIcon-appreciatefill margin-lr-xs"></text> {{data.supportNumber}}
+					<text class="cuIcon-messagefill margin-lr-xs"></text> {{data.commentNumber}}
+				</view>
+
+				<view class="cu-list menu-avatar comment solids-top">
+					<view class="cu-item">
+						<view class="cu-avatar round" style="background-image:url(https://zyx-max.oss-cn-beijing.aliyuncs.com/myhead.jpg);"></view>
+						<view class="content">
+							<view class="text-grey">左头</view>
+							<view class="text-gray text-content text-df">
+								我们还行吧
+							</view>
+							<view class="bg-grey padding-sm radius margin-top-sm  text-sm">
+								<view class="flex">
+									<view>右头：</view>
+									<view class="flex-sub">那当然</view>
+								</view>
+							</view>
+							<view class="margin-top-sm flex justify-between">
+								<view class="text-gray text-df">2019年8月30日</view>
+								<view>
+									<text class="cuIcon-appreciatefill text-red"></text>
+									<text class="cuIcon-messagefill text-gray margin-left-sm"></text>
+								</view>
+							</view>
+						</view>
+					</view>
+
+					<view class="cu-item">
+						<view class="cu-avatar round" style="background-image:url(https://zyx-max.oss-cn-beijing.aliyuncs.com/myhead.jpg);"></view>
+						<view class="content">
+							<view class="text-grey">右头</view>
+							<view class="text-gray text-content text-df">
+								往那边走
+							</view>
+							<view class="bg-grey padding-sm radius margin-top-sm  text-sm">
+								<view class="flex">
+									<view>左头：</view>
+									<view class="flex-sub">不！是那边</view>
+								</view>
+							</view>
+							<view class="margin-top-sm flex justify-between">
+								<view class="text-gray text-df">2019年8月30日</view>
+								<view>
+									<text class="cuIcon-appreciate text-gray"></text>
+									<text class="cuIcon-messagefill text-gray margin-left-sm"></text>
+								</view>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="box add-comment discuss-comment-add">
+			<view class="cu-bar input">
+				<view class="cu-avatar round" :style= '"background-image:url(" + user_avatar +" );"'></view>
+				<view class="action">
+					<text class="cuIcon-roundaddfill text-grey"></text>
+				</view>
+				<input class="solid-bottom" maxlength="20" cursor-spacing="10" @blur="(e) => {this.pushInfo(e)}" :value="comment"></input>
+				<view class="action">
+					<text class="cuIcon-emojifill text-grey"></text>
+				</view>
+				<button class="cu-btn bg-green shadow-blur" @click="insertComment">发送</button>
+			</view>
+		</view>
+			
 	</view>
 </template>
 
@@ -44,6 +130,10 @@
 					tempData.content = data.content;
 					tempData.contentImg = JSON.parse(data.content_img);
 					tempData.user_avatar = data.avatar_url;
+					tempData.userName = data.name;
+					tempData.vivewNumber = data.vivew_number;
+					tempData.supportNumber = data.support_number;
+					tempData.commentNumber = data.comment_number;
 					_self.data =  tempData;
 			    }
 			});
@@ -55,5 +145,13 @@
 </script>
 
 <style>
+	.cu-card>.cu-item {
+		margin-bottom: 120rpx!important;
+	}
+	.discuss-comment-add{
+		position: fixed;
+		bottom:0rpx;
+		width: 100%;
+	}
 </style>
 
